f2be6b01d9b10ed39c5eebbf02d3a7b73dd1498e
George Whiting <george_whiting@hotmail.com>
2018-02-25 22:45:51 +0000

    remove account_holder initialisation test and attr_reader for account details to keep data obscured

--- lib/account_holder.rb
+++ lib/account_holder.rb
@@ -1,7 +1,5 @@
 class Account_holder
 
-  attr_reader :account_details
-
   def initialize(account_details)
     @account_details = account_details
   end
--- spec/account_holder_spec.rb
+++ spec/account_holder_spec.rb
@@ -10,16 +10,16 @@ describe Account_holder do
   }
   subject {described_class.new(test_details)}
 
-  describe '#account_details' do
-    it 'should be initialized with a hash of details' do
-      expect(subject.account_details['id']).to eq 'testguid'
-      expect(subject.account_details['firstname']).to eq 'first'
-      expect(subject.account_details['lastname']).to eq 'last'
-      expect(subject.account_details['email']).to eq 'test@test.com'
-      expect(subject.account_details['telephone']).to eq '0123456789'
-      expect(subject.account_details['balance']).to eq '100.00'
-    end
-  end
+  # describe '#account_details' do
+  #   it 'should be initialized with a hash of details' do
+  #     expect(subject.account_details['id']).to eq 'testguid'
+  #     expect(subject.account_details['firstname']).to eq 'first'
+  #     expect(subject.account_details['lastname']).to eq 'last'
+  #     expect(subject.account_details['email']).to eq 'test@test.com'
+  #     expect(subject.account_details['telephone']).to eq '0123456789'
+  #     expect(subject.account_details['balance']).to eq '100.00'
+  #   end
+  # end
 
   describe '#display_balance' do
     it "should return the account's balance" do
