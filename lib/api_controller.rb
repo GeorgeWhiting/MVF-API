66a40e03729deef01a322179adecb6c269925113
George Whiting <george_whiting@hotmail.com>
2018-02-25 22:18:48 +0000

    alter verify_user to create a new account_holder class if a valid guid is entered

--- lib/api_controller.rb
+++ lib/api_controller.rb
@@ -2,16 +2,23 @@ require 'json'
 
 class Api_controller
 
-  attr_reader :account_list
+  attr_reader :account_list, :account
 
-  def initialize
+  def initialize(account_class = Account_holder)
+    @account_class = account_class
+    @account = ''
+
     file = File.read('a4a06bb0-3fbe-40bd-9db2-f68354ba742f.json')
     @account_list = JSON.parse(file)['accounts']
   end
 
   def verify_user(guid)
     valid_accounts = @account_list.select{ |account| account['id'] == guid}
-    valid_accounts.length == 1
+    if valid_accounts.length == 1
+      @account = @account_class.new(valid_accounts.first)
+    else
+      'This guid does not exist'
+    end
   end
 
 end
--- spec/api_controller_spec.rb
+++ spec/api_controller_spec.rb
@@ -1,6 +1,9 @@
 describe Api_controller do
 
-  subject {described_class.new}
+  subject {described_class.new(account_class)}
+
+  let(:account_holder) { double(:account_holder, display_balance: '100.00') }
+  let(:account_class) { double(:account_class, new: account_holder) }
 
   describe '#account_list' do
     it 'should return an array of the accounts in the data file' do
@@ -10,12 +13,15 @@ describe Api_controller do
   end
 
   describe '#verify_user' do
-    it 'should return true if the entered guid matches an account guid' do
-      expect(subject.verify_user('50456415-4ea0-42b2-adae-063edce3225c')).to eq true
+    it 'should return a new account object if the entered guid matches an account guid' do
+      expect(account_class).to receive(:new)
+      subject.verify_user('50456415-4ea0-42b2-adae-063edce3225c')
+      expect(subject.account.display_balance).to eq '100.00'
     end
-    it 'should return false if the entered guid does not matche an account guid' do
-      expect(subject.verify_user('testguid')).to eq false
+    it 'should return a warning if the entered guid does not matche an account guid' do
+      expect(subject.verify_user('testguid')).to eq 'This guid does not exist'
     end
   end
+
 
 end
