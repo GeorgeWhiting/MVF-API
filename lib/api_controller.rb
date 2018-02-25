acf76b0515f19ae88b68dce359ceb1549208193f
George Whiting <george_whiting@hotmail.com>
2018-02-25 16:49:35 +0000

    initialize api_controller class with array of accounts

--- lib/api_controller.rb
+++ lib/api_controller.rb
@@ -1,4 +1,13 @@
+require 'json'
+
 class Api_controller
 
+  attr_reader :account_list
+
+  def initialize
+    file = File.read('a4a06bb0-3fbe-40bd-9db2-f68354ba742f.json')
+    parsed_json = JSON.parse(file)
+    @account_list = parsed_json['accounts']
+  end
 
 end
--- spec/api_controller_spec.rb
+++ spec/api_controller_spec.rb
@@ -1,4 +1,12 @@
 describe Api_controller do
-  
+
+  subject {described_class.new}
+
+  describe '#account_list' do
+    it 'should return an array of the accounts in the data file' do
+      expect(subject.account_list.length).to eq 100
+      expect(subject.account_list).to be_an_instance_of(Array)
+    end
+  end
 
 end
