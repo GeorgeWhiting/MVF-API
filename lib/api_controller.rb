20b1d4917f099f5121d0e8fc29001e5babf3e5db
George Whiting <george_whiting@hotmail.com>
2018-02-26 18:32:18 +0000

    add extract_account_guids method to ApiController. Modify verify user to validate customers based on the array created by parse_bucket

--- lib/api_controller.rb
+++ lib/api_controller.rb
@@ -3,34 +3,46 @@ require 'httparty'
 
 class ApiController
   include HTTParty
-  attr_reader :account_list, :account, :customer
+  attr_reader :account, :customer, :account_list
 
   def initialize(account_class = AccountHolder, customer_class = Customer)
+    @base_uri = 'https://mvf-devtest-s3api.s3-eu-west-1.amazonaws.com/'
     @account_class = account_class
     @customer_class = customer_class
     @account = ''
     @customer = ''
-
-    @base_uri = 'https://mvf-devtest-s3api.s3-eu-west-1.amazonaws.com/'
-
-    file = File.read('a4a06bb0-3fbe-40bd-9db2-f68354ba742f.json')
-    @account_list = JSON.parse(file)['accounts']
+    @customer_guids = parse_bucket
+    @account_list = []
+    extract_account_guids
   end
 
   def parse_bucket
-    matchdata = HTTParty.get(@base_uri).body.scan(/<Key>([^><]*)<\/Key>/).flatten
-    guids = matchdata.map { |x| x.delete(".json") }
+    @customer_guids = isolate_guids.map { |x| x.delete(".json") }
   end
 
   def verify_user(guid)
-    if guid == 'a4a06bb0-3fbe-40bd-9db2-f68354ba742f'
-      @customer = @customer_class.new(account_list)
+    if @customer_guids.include?(guid)
+      @customer = @customer_class.new(@account_list)
+      return
     end
     valid_accounts = @account_list.select { |account| account['id'] == guid }
-    if valid_accounts.length == 1
+    if !valid_accounts.empty?
       @account = @account_class.new(valid_accounts.first)
     else
       'This guid does not exist'
     end
   end
+
+  def extract_account_guids
+    @customer_guids.each do |customer|
+      @account_list += JSON.parse(self.class.get("#{@base_uri}#{customer}.json"))['accounts']
+    end
+  end
+
+  private
+
+  def isolate_guids
+    HTTParty.get(@base_uri).body.scan(/<Key>([^><]*)<\/Key>/).flatten
+  end
+
 end
--- spec/api_controller_spec.rb
+++ spec/api_controller_spec.rb
@@ -6,26 +6,26 @@ describe ApiController do
   let(:account_holder) { double(:account_holder, display_balance: '100.00') }
   let(:account_class) { double(:account_class, new: account_holder) }
 
-  describe '#account_list' do
-    it 'should return an array of the accounts in the data file' do
-      expect(subject.account_list.length).to eq 100
-      expect(subject.account_list).to be_an_instance_of(Array)
-    end
+  before(:each) do
+    allow(subject).to receive(:isolate_guids).and_return(['a4a06bb0-3fbe-40bd-9db2-f68354ba742f.json', 'be0438bf-8b0d-4c57-913d-fcafb0bb41f0.json', 'be9b2a8b-e846-4365-8d5f-0fca4ef9aefb.json'])
   end
 
   describe 'parse_bucket' do
     it 'should exctract all guids from the AWS bucket and store them in an array' do
-      expect(subject.parse_bucket).to eq ['123', '321']
+      expect(subject.parse_bucket).to eq ['a4a06bb0-3fbe-40bd-9db2-f68354ba742f', 'be0438bf-8b0d-4c57-913d-fcafb0bb41f0', 'be9b2a8b-e846-4365-8d5f-0fca4ef9aefb']
     end
   end
 
   describe '#verify_user' do
     it 'should return a new account object if the entered guid matches an account guid' do
+      subject.parse_bucket
+      subject.extract_account_guids
       expect(account_class).to receive(:new)
       subject.verify_user('50456415-4ea0-42b2-adae-063edce3225c')
       expect(subject.account.display_balance).to eq '100.00'
     end
     it 'should return a new customer object if the entered guid matches a customer guid' do
+      subject.parse_bucket
       expect(customer_class).to receive(:new)
       subject.verify_user('a4a06bb0-3fbe-40bd-9db2-f68354ba742f')
     end
