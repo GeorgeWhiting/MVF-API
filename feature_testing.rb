7c34996d1194a4e294332ff9c7859c821b550a39
George Whiting <george_whiting@hotmail.com>
2018-02-26 14:50:07 +0000

    update feature test file for customer interactions

--- feature_testing.rb
+++ feature_testing.rb
@@ -1,13 +1,15 @@
 require_relative 'lib/api_controller'
 require_relative 'lib/account_holder'
+require_relative 'lib/customer'
 
-api = Api_controller.new
+api = ApiController.new
+
+api.verify_user('a4a06bb0-3fbe-40bd-9db2-f68354ba742f')
+p api.customer.accounts_in_debt
+p api.customer.account_details('9c4ce9dc-92ed-49d0-ad3f-ce634d74483e')
+p '---------------------------------'
 api.verify_user('8a28f09a-c234-4a95-b1e0-cdbc68979d0a')
-
-# p api.verify_user('wrong')
-
-p api.account
-
-#
-# p api.account.display_balance
-# p api.account.show_account_details
+p api.account.display_balance
+p api.account.show_account_details
+p '---------------------------------'
+p api.verify_user('wrong')
