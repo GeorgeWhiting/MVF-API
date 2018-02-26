b835db665f68546563f4fe71511c0328cebf21d9
George Whiting <george_whiting@hotmail.com>
2018-02-26 14:51:41 +0000

    update verify_user in apiController class to accept customer guids and create new instances of customers. At this stage, the verification is somewhat invalid as the app parses the customer file by default. This will change as soon as the whole AWS bucket is introduced

--- lib/api_controller.rb
+++ lib/api_controller.rb
@@ -1,17 +1,22 @@
 require 'json'
 
 class ApiController
-  attr_reader :account_list, :account
+  attr_reader :account_list, :account, :customer
 
-  def initialize(account_class = AccountHolder)
+  def initialize(account_class = AccountHolder, customer_class = Customer)
     @account_class = account_class
+    @customer_class = customer_class
     @account = ''
+    @customer = ''
 
     file = File.read('a4a06bb0-3fbe-40bd-9db2-f68354ba742f.json')
     @account_list = JSON.parse(file)['accounts']
   end
 
   def verify_user(guid)
+    if guid == 'a4a06bb0-3fbe-40bd-9db2-f68354ba742f'
+      @customer = @customer_class.new(account_list)
+    end
     valid_accounts = @account_list.select { |account| account['id'] == guid }
     if valid_accounts.length == 1
       @account = @account_class.new(valid_accounts.first)
--- spec/api_controller_spec.rb
+++ spec/api_controller_spec.rb
@@ -1,6 +1,8 @@
 describe ApiController do
-  subject { described_class.new(account_class) }
+  subject { described_class.new(account_class, customer_class) }
 
+  let(:customer) { double(:customer) }
+  let(:customer_class) {double(:customer_class, new: customer) }
   let(:account_holder) { double(:account_holder, display_balance: '100.00') }
   let(:account_class) { double(:account_class, new: account_holder) }
 
@@ -16,6 +18,10 @@ describe ApiController do
       expect(account_class).to receive(:new)
       subject.verify_user('50456415-4ea0-42b2-adae-063edce3225c')
       expect(subject.account.display_balance).to eq '100.00'
+    end
+    it 'should return a new customer object if the entered guid matches a customer guid' do
+      expect(customer_class).to receive(:new)
+      subject.verify_user('a4a06bb0-3fbe-40bd-9db2-f68354ba742f')
     end
     it 'should return a warning if the entered guid does not matche an account guid' do
       expect(subject.verify_user('testguid')).to eq 'This guid does not exist'
