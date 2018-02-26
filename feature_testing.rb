da424fdd4f2fe00da847a5de462ef1f49714120f
George Whiting <george_whiting@hotmail.com>
2018-02-26 18:28:00 +0000

    update feature test file to show all user options

--- feature_testing.rb
+++ feature_testing.rb
@@ -5,7 +5,7 @@ require_relative 'lib/customer'
 api = ApiController.new
 
 api.verify_user('a4a06bb0-3fbe-40bd-9db2-f68354ba742f')
-p api.customer.accounts_in_debt
+p api.customer.accounts_in_debt.length
 p api.customer.account_details('9c4ce9dc-92ed-49d0-ad3f-ce634d74483e')
 p '---------------------------------'
 api.verify_user('8a28f09a-c234-4a95-b1e0-cdbc68979d0a')
